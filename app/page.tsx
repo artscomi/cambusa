c8bc0c5d00ff2d9e18f1621694231a50f8bf6843
artscomi <artscomi.web@gmail.com>
2024-09-15 12:59:45 +0200

    feat(): mobile layout hp

--- app/page.tsx
+++ app/page.tsx
@@ -14,30 +14,33 @@ export default function Home() {
   return isPending ? (
     <Loading />
   ) : (
-    <div className="relative">
-      <div className="max-md:p-5 mb-5 md:mb-16">
-        <h1 className="text-5xl mb-4">Cambusa AI</h1>
-        <p className="text-xl text-left text-pretty">
-          Crea il menu perfetto per tutto l&apos;equipaggio. Siete pronti a
-          salpare? ⛵
-        </p>
-      </div>
-
-      <div className="w-full items-end">
-        <div className="flex flex-col md:flex-row justify-items-center gap-16 items-center  md:rounded">
-          <div className="flex-1 w-full">
-            <MainForm startTransition={startTransition} setError={setError} />
-          </div>
-          <div className="flex-1 w-full">
-            <CreateGroupBox />
-          </div>
+    <>
+      <div className="h-screen max-sm:flex flex-col justify-center">
+        <div className="max-md:p-5 mb-5 md:mb-16">
+          <h1 className="text-5xl mb-4">Cambusa AI</h1>
+          <p className="text-xl text-left text-pretty">
+            Crea il menu perfetto per tutto l&apos;equipaggio. Siete pronti a
+            salpare? ⛵
+          </p>
         </div>
-        {/* <MealMenu /> */}
-        {/* <AddMenu /> */}
 
-        
+        <div className="w-full items-end">
+          <div className="flex flex-col md:flex-row justify-items-center gap-16 items-center">
+            <div className="flex-1 w-full">
+              <MainForm startTransition={startTransition} setError={setError} />
+            </div>
+            <div className="flex-1 w-full max-sm:hidden">
+              <CreateGroupBox />
+            </div>
+          </div>
+          {/* <MealMenu /> */}
+          {/* <AddMenu /> */}
+        </div>
+        <ToastError error={error} setError={setError} />
       </div>
-      <ToastError error={error} setError={setError} />
-    </div>
+      <div className="sm:hidden pt-20">
+        <CreateGroupBox />
+      </div>
+    </>
   );
 }
--- components/MainForm/index.tsx
+++ components/MainForm/index.tsx
@@ -117,7 +117,7 @@ export const MainForm = ({
         </form>
 
 
-        <div className="absolute left-0 right-0 top-0 bottom-0 overflow-hidden">
+        <div className="absolute left-0 right-0 top-0 bottom-0 sm:overflow-hidden">
           <LottieAnimation name="waveBig" />
         </div>
       </div>
