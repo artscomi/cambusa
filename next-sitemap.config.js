10aa696e64d632eade208bfa1973e58bd7ddb82c
artscomi <artscomi.web@gmail.com>
2024-11-03 22:58:52 +0100

    feat(): add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: "https://yourdomain.com", // Replace with your domain
+  generateRobotsTxt: true, // Automatically generate a robots.txt file
+  sitemapSize: 7000, // Adjust the size if you have many URLs
+};
