827cf5b290e4ec62e444b20e74100a6e646809f2
artscomi <artscomi.web@gmail.com>
2024-11-06 14:58:23 +0100

    feat(): add robot.tsx

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)", "/sitemap.xml"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/sitemap.xml",
+  "/robots.txt",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://yourdomain.com", // Replace with your domain
+  siteUrl: "https://www.cambusa-online.com", // Replace with your domain
   generateRobotsTxt: true, // Automatically generate a robots.txt file
   sitemapSize: 7000, // Adjust the size if you have many URLs
 };
