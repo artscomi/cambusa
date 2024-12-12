f3dff0b539b28d15a4d4bcfa2d98aebadb5c5a6b
artscomi <artscomi-web@google.com>
2024-12-12 15:43:07 +0100

    feat: add loading state to createGroup cta

--- app/group/create-group/CreateGroupForm.tsx
+++ app/group/create-group/CreateGroupForm.tsx
@@ -5,25 +5,34 @@ import { TextInput } from "@/components/TextInput";
 import { createGroupAction } from "@/app/api/actions";
 import { useFormConfig } from "@/hooks/useFormConfig";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const CreateGroupForm = () => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const router = useRouter();
   const { inputConfig } = useFormConfig();
+  const [loading, setLoading] = useState(false);
 
-  const handleCreateGroup = async (formData: FormData) => {
-    const groupId = await createGroupAction(formData);
-
-    if (groupId) {
-      router.push(`${baseUrl}/group/${groupId}/`);
-    } else {
-      console.error("Error creating group");
+  const handleCreateGroup = async (e: React.FormEvent<HTMLFormElement>, formData: FormData) => {
+    e.preventDefault()
+    try {
+      setLoading(true);
+      const groupId = await createGroupAction(formData);
+      if (groupId) {
+        router.push(`${baseUrl}/group/${groupId}/`);
+      } else {
+        console.error("Error creating group");
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="md:rounded-lg p-8 md:p-14 md:shadow-md relative max-sm:-mx-4 md:overflow-hidden bg-white">
-      <form action={handleCreateGroup}>
+      <form onSubmit={e => handleCreateGroup(e, new FormData(e.target as HTMLFormElement))}>
         <div className="grid gap-3">
           {inputConfig.map((config) => (
             <TextInput key={config.id} {...config} />
@@ -31,7 +40,7 @@ export const CreateGroupForm = () => {
         </div>
 
         <Button type="submit" className="my-10" center>
-          Crea un gruppo
+          {loading ? "Salvataggio..." : "Crea un gruppo"}
         </Button>
       </form>
     </div>
