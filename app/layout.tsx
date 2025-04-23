d998a43bdaa9b6e03bae1667139861ce3fc64929
artscomi <artscomi.web@gmail.com>
2025-04-23 13:12:16 +0200

    feat: add hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,19 @@ export default function RootLayout({
       >
         <head>
           <meta name="theme-color" content="#0895c5" />
+          {/* Hotjar */}
+          <Script id="hotjar" strategy="afterInteractive">
+            {`
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6380619,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+          </Script>
           {isProduction && (
             <Script
               id="clarity-script"
