bde10a3ec86bb1b98d06f19e67bcd0de73bb93e1
artscomi <artscomi.web@gmail.com>
2025-04-27 12:09:48 +0200

    feat: update next-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,20 @@
+// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.cambusa-online.com',
+  siteUrl: 'https://www.cambusa-online.com', // tuo dominio
   generateRobotsTxt: true,
-  // altre opzioni
-}
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+  exclude: [], // se vuoi escludere delle route tipo '/admin'
+  transform: async (config, path) => {
+    // Customizzazione dei campi nella sitemap
+    return {
+      loc: path, // URL della pagina
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
