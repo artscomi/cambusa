a68a57c8e2026e3a9a74af9551eb900022f73482
artscomi <artscomi.web@gmail.com>
2025-04-27 11:55:09 +0200

    feat: update next-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://cambusa-online.com',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: 'https://www.cambusa-online.com',
+  generateRobotsTxt: true,
+  // altre opzioni
 }
