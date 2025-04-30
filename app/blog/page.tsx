a3807d08a2cd000373874feda57036b56b0d8026
artscomi <artscomi.web@gmail.com>
2025-04-30 17:05:05 +0200

    fix: update import path for BlogCta component to reflect new directory structure

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import { Metadata } from "next";
-import { BlogCta } from "./pasti-in-barca/cta";
+import { BlogCta } from "./pasti-in-barca-cambusa/cta";
 
 export const metadata: Metadata = {
   title: "Blog | Cambusa Online",
