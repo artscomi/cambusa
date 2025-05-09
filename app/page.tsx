6c781f15569b3ee5dad18e3a2edc075bfc114693
artscomi <artscomi.web@gmail.com>
2025-05-09 21:54:19 +0200

    fix: update text for clarity in Home component and adjust label in useFormConfig hook

--- app/page.tsx
+++ app/page.tsx
@@ -71,9 +71,9 @@ export default function Home() {
             salpare? ⛵
           </p>
           <p className="text-sm sm:text-base text-pretty font-subtitle text-gray-500 mb-4">
-            Con cambusaai puoi creare un menu personalizzato, modificare i
-            piatti e generare una lista cambusa completa per una settimana in
-            barca.
+            Con cambusaai puoi creare un menu personalizzato in base alle
+            preferenze alimentari di tutto l'equipaggio, modificare i piatti e
+            generare una lista della spesa per una settimana in barca.
           </p>
         </div>
 
--- hooks/useFormConfig.tsx
+++ hooks/useFormConfig.tsx
@@ -118,7 +118,7 @@ export const useFormConfig = (isSimpleFlow?: boolean) => {
     {
       name: "people",
       id: "people",
-      label: "Quante persone siete?",
+      label: "Quanti siete a bordo?",
       value: formState.people,
       placeholder: "5",
       type: "number",
