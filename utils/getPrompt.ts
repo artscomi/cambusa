cafafb155d89e7c24c8568e9a794ebfcba3901eb
artscomi <artscomi.web@gmail.com>
2025-06-03 00:38:31 +0200

    refactor: improve prompt formatting in getMainPrompt function for better readability

--- utils/getPrompt.ts
+++ utils/getPrompt.ts
@@ -7,9 +7,12 @@ export const getMainPrompt = ({
   dinner,
   dietaryPreferences,
   people,
-}: FormState): string => `Crea una proposta di menu in italiano con ${breakfast || 0} colazioni, ${lunch} pranzi, ${dinner} cene per ${people} persone.
+}: FormState): string => `Crea una proposta di menu in italiano con ${
+  breakfast || 0
+} colazioni, ${lunch} pranzi, ${dinner} cene per ${people} persone.
   Preferenze alimentari includono: pasta circa 100 grammi a testa, ${dietaryPreferences}. Cerca di accontentare tutti, senza proporre piatti diversi per ognuno, tranne a Colazione. Non includere nel menu integratori alimentari. Cerca di proporre carboidrati a pranzo e proteine a cena.
   I piatti devono essere adatti alla preparazione in barca, quindi privilegia preparazioni semplici ed evita l'uso del forno.
+  Per le colazioni, usa lo stesso menu per tutti i giorni.
  Genera un oggetto JSON che rappresenti un piano alimentare. L'oggetto deve avere tre categorie principali: "colazioni", "pranzi" e "cene".
 Per ogni categoria, includi il numero di pasti indicato.
 Ogni pasto deve avere almeno un piatto, e ogni piatto deve contenere 2-4 ingredienti.
