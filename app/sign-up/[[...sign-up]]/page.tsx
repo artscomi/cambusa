a74ee41b872028fe323f49723ce15811c2702910
artscomi <artscomi.web@gmail.com>
2025-06-03 12:27:22 +0200

    feat: enhance SignUp component with custom appearance settings for terms and privacy pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,14 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <SignUp
+      appearance={{
+        layout: {
+          termsPageUrl: "/termini",
+          privacyPageUrl: "/privacy",
+        },
+      }}
+    />
+  );
 }
